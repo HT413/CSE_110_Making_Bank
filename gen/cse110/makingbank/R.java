25321f8ae34c593b78465e3d06d1b11bb6b5932c
Reiyx <cindy.ly@aol.com>
2014-10-28 10:15:27 -0700

    Created a round corner border for data fields

--- gen/cse110/makingbank/R.java
+++ gen/cse110/makingbank/R.java
@@ -49,6 +49,7 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int placeholder=0x7f020001;
+        public static final int round_corners=0x7f020002;
     }
     public static final class id {
         public static final int EditTextAddress1=0x7f0a0006;
